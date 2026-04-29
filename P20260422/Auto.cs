5622c947dd7910ac7f4aec5bed6e02b3c7ffe792
TakacsAdam92 <135602565+TakacsAdam92@users.noreply.github.com>
2026-04-29 10:54:20 +0200

    Add files via upload

---
+++ P20260422/Auto.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P20260422
+{
+    internal class Auto
+    {
+        int Sorszam;
+        string Marka;
+        string Modell;
+        int Gyartasi_ev;
+        string Szin;
+        int Eladottdb;
+        double AtlageladasiAr;
+
+        public Auto(int sorszam, string marka, string modell, int gyartasi_ev, string szin, int eladottdb, double atlageladasiAr)
+        {
+            Sorszam = sorszam;
+            Marka = marka;
+            Modell = modell;
+            Gyartasi_ev = gyartasi_ev;
+            Szin = szin;
+            Eladottdb = eladottdb;
+            AtlageladasiAr = atlageladasiAr;
+        }
+
+        public int Sorszam1 { get => Sorszam; set => Sorszam = value; }
+        public string Marka1 { get => Marka; set => Marka = value; }
+        public string Modell1 { get => Modell; set => Modell = value; }
+        public int Gyartasi_ev1 { get => Gyartasi_ev; set => Gyartasi_ev = value; }
+        public string Szin1 { get => Szin; set => Szin = value; }
+        public int Eladottdb1 { get => Eladottdb; set => Eladottdb = value; }
+        public double AtlageladasiAr1 { get => AtlageladasiAr; set => AtlageladasiAr = value; }
+
+        public static List<Auto> beolvas(string fnev)
+        {
+           List<Auto> autoLista = new List<Auto>();
+           string[] sorok = File.ReadAllLines("fnev");
+           return autoLista;
+        }
+    }
+}
---
+++ P20260422/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace P20260422
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            dataGrid.ItemsSource.(Auto.beolvas("autok.csv"));
+            dataGrid.Items.Add();
+        }
+    }
+}
